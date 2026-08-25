ab6e2a3e10d8862ae1994cb3a283ceb43c02afd2
KorayAkc <koray.akcay.outlook.com@zbw-online.ch>
2026-08-25 21:11:10 +0200

    Persistenz-Schicht hinzugefügt (DataStore)

---
+++ ContactManager/Data/DataStore.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using ContactManager.Models;
+
+namespace ContactManager.Data
+{
+    /// <summary>
+    /// Hält den gesamten Datenstamm der Anwendung im Speicher.
+    /// Wird als Ganzes serialisiert und wieder geladen.
+    /// </summary>
+    public class DataStore
+    {
+        /// <summary>Alle erfassten Kunden.</summary>
+        public List<Kunde> Kunden { get; set; } = new();
+
+        /// <summary>Alle erfassten Mitarbeiter.</summary>
+        public List<Mitarbeiter> Mitarbeiter { get; set; } = new();
+    }
+}
